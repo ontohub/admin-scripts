1ff6faf6b490c10684dda580df1aa43d037dbf3a
Tim Reddehase <tredd@informatik.uni-bremen.de>
2014-11-03 09:52:56 +0100

    make ontohub-console callable from root/ontohub
    The script should be executable from root
    and ontohub without any problems.

--- scripts/ontohub-console.sh
+++ scripts/ontohub-console.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-su - ontohub -c 'cd /srv/http/ontohub/current && bundle exec rails c production'
+#!/bin/bash
+
+if [[ "$(whoami)" != 'ontohub' ]]; then
+  su - ontohub -c 'cd /srv/http/ontohub/current && bundle exec rails c production'
+else
+  cd /srv/http/ontohub/current && bundle exec rails c production
+fi
